3e5d41c1a06cb1632ec72b129eee888d133f1118
herbertDev12 <herbertn740@gmail.com>
2025-04-15 14:31:29 -0700

    Changed the position of CompletionButton in TaskRow at the begining of the row

--- Components/TaskRow.tsx
+++ Components/TaskRow.tsx
@@ -3,10 +3,8 @@ import CompletionButton from "./CompletionButton"
 export default function TaskRow(){
     return(
         <div>
-            <th style={{ border: '4px double #009688', padding: '8px' }}>(Task1)</th>
-            <CompletionButton>
-                
-            </CompletionButton>
+            
+            <th style={{ border: '4px double #009688', padding: '8px' }}><CompletionButton/> (Task1)  </th>           
         </div>
         
     )
