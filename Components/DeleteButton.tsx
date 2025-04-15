5b7b888ac8eaed5575d8acd4db7fe60baf10f3ed
herbertDev12 <herbertn740@gmail.com>
2025-04-15 14:49:30 -0700

    Implemented what the DeleteButton will render

---
+++ Components/DeleteButton.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function DeleteButton(){
+    return (
+        <button>'🗑️'</button>
+    );
+}
--- Components/TaskDoneRow.tsx
+++ Components/TaskDoneRow.tsx
@@ -1,5 +1,7 @@
+
+
 export default function TaskDoneRow(){
     return(
-        <th style={{ border: '4px double #009688', padding: '8px' }}>(Task1)</th>     
+        <th style={{ border: '4px double #009688', padding: '8px' }}>(Task1)  </th>       
     )
 }
