518ee898f85893f85cbcc8d9c95fe0537b68d5a7
herbertDev12 <herbertn740@gmail.com>
2025-04-15 17:12:00 -0700

    Implemented the handledAddTask and we pass some props to the child components

--- Components/ToDoTable.tsx
+++ Components/ToDoTable.tsx
@@ -1,47 +1,39 @@
-import TaskRow from "./TaskRow";
-import { useState } from "react";
+import { useState } from 'react';
+import AddBar from './AddBar';
+import TaskRow from './TaskRow';
 
-export default function ToDoTable(){
+interface Task {
+  id: number;
+  name: string;
+}
 
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setTask] = useState('');
+export default function ToDoTable() {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTask, setNewTask] = useState('');
+
+    const handleAddTask = () => {
+        if (newTask.trim()) {
+        setTasks([...tasks, { id: Date.now(), name: newTask }]);
+        setNewTask('');
+        }
+    };
 
     return (
         <div>
-            
-            <table style={{
-                border: '4px double rgba(255, 255, 255, 0.7)',
-                padding: '8px',
-                background: 'rgba(255, 255, 255, 0.1)',
-                color: 'white',
-                textShadow: '0 0 3px rgba(255, 255, 255, 0.5)',
-                borderCollapse: 'collapse',
-                width: '500px',
-                margin: '20px 0',
-                position: "absolute",
-                top: "300px",
-                left: "20px"
-            }}>
-                <thead>
-                    <tr>
-                        <th style={{ 
-                            border: '4px double rgba(255, 255, 255, 0.7)', 
-                            padding: '8px',
-                            background: 'rgba(21, 15, 15, 0.1)',
-                            color: 'white',
-                            textShadow: '0 0 3px rgba(18, 17, 17, 0.5)',
-                        }}>      To Do       </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <TaskRow></TaskRow>                        
-                    </tr>
-                    <tr>
-                        <TaskRow></TaskRow>     
-                    </tr>
-                </tbody>
-            </table>
+          <AddBar 
+            newTask={newTask} 
+            setNewTask={setNewTask} 
+            onAdd={handleAddTask} 
+          />
+          
+          <table style={{ marginTop: '120px', marginLeft: '750px' }}>
+            <tbody>
+              {tasks.map((task) => (
+                <TaskRow key={task.id} taskName={task.name} />
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
-}
+      );
+    }
+
