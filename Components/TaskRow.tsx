ec2e3d2362798f4ea9c7de3e8e64ab227fa84ed5
herbertDev12 <herbertn740@gmail.com>
2025-04-15 17:55:52 -0700

    Implemented the interactivity an logic in TaskRow component

--- Components/TaskRow.tsx
+++ Components/TaskRow.tsx
@@ -1,20 +1,25 @@
-import CompletionButton from "./CompletionButton"
-import DeleteButton from "./DeleteButton"
+import CompletionButton from "./CompletionButton";
+import DeleteButton from "./DeleteButton";
 
-export default function TaskRow() {
-    return (
-      <div>
-        <th style={{ 
-          border: '4px double #009688', 
-          padding: '8px',
-          display: 'flex',
-          alignItems: 'center',
-          gap: '160px' 
-        }}>
-          <CompletionButton/>
-          <span>(Task1)</span>
-          <DeleteButton/>
-        </th>           
-      </div>
-    );
-  }
+interface TaskRowProps {
+  taskName: string;
+}
+
+export default function TaskRow({taskName} : TaskRowProps ) {
+  return (
+    <tr>
+      <td style={{ 
+        border: '4px double #009688', 
+        padding: '8px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '20px',
+        margin: '5px 0'
+      }}>
+        <CompletionButton/>
+        <span style={{ minWidth: '120px' }}>{taskName}</span>
+        <DeleteButton/>
+      </td>           
+    </tr>
+  );
+}
