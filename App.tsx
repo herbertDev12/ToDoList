dd26ad50b8cadd782af0072bff3bdbabb538a766
herbertDev12 <herbertn740@gmail.com>
2025-04-16 15:08:53 -0700

    Implemented the logic of InProgTable and deleted the AddButton

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Title from './Components/Title'
 import AddBar from './Components/AddBar';
-import AddButton from './Components/AddButton';
 import ToDoTable from './Components/ToDoTable';
 import DoneTable from './Components/DoneTable';
 import InProgTable from './Components/InProgTable';
@@ -11,7 +10,6 @@ const App: React.FC = () =>{
     <div className = "App">
       <Title/>
       <AddBar/>
-      <AddButton/>
       <ToDoTable/>
       <DoneTable/>
       <InProgTable/>
--- Components/InProgTable.tsx
+++ Components/InProgTable.tsx
@@ -1,36 +1,38 @@
 import TaskRow from "./TaskRow";
+import AddBar from "./AddBar";
+import { useState } from "react";
+
+interface Task{
+  id: number;
+  name: string;
+}
 
 export default function InProgTable(){
-    return (
-        <div>
-            
-            <table style={{
-                border: '4px double rgba(255, 255, 255, 0.7)',
-                padding: '8px',
-                background: 'rgba(255, 255, 255, 0.1)',
-                color: 'white',
-                textShadow: '0 0 3px rgba(255, 255, 255, 0.5)',
-                borderCollapse: 'collapse',
-                width: '500px',
-                margin: '20px 0',
-                position: "absolute",
-                top: "300px",
-                left: "650px",
-            }}>
-                <thead>
-                    <tr>
-                        <th style={{ border: '4px double #009688', padding: '8px' }}>      In Progress       </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <TaskRow></TaskRow>                        
-                    </tr>
-                    <tr>
-                        <TaskRow></TaskRow> 
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState('');
+  
+  const handleAddTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, { id: Date.now(), name: newTask }]);
+      setNewTask('');
+    }
+   };
+  return (
+    <div>
+      <AddBar 
+        newTask={newTask} 
+        setNewTask={setNewTask} 
+        onAdd={handleAddTask} 
+        />
+                  
+        <table style={{ marginTop: '120px', marginLeft: '750px' }}>
+           <tbody>
+              {tasks.map((task) => (
+                <TaskRow key={task.id} taskName={task.name} />
+              ))}
+            </tbody>
+        </table>
+      </div>
+  );
 }
+  
