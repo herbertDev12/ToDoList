0134f2b57091cbe932810208f79c69b2a70915a0
herbertDev12 <herbertn740@gmail.com>
2025-04-16 15:22:56 -0700

    Deleted the AddBar from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Title from './Components/Title'
-import AddBar from './Components/AddBar';
 import ToDoTable from './Components/ToDoTable';
 import DoneTable from './Components/DoneTable';
 import InProgTable from './Components/InProgTable';
+
+
 
 const App: React.FC = () =>{
   return (
     <div className = "App">
       <Title/>
-      <AddBar/>
+      
       <ToDoTable/>
       <DoneTable/>
       <InProgTable/>
