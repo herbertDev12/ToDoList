937b4b67653fdac70ad40557e18c99bed577f0c4
herbertDev12 <herbertn740@gmail.com>
2025-04-15 17:36:00 -0700

    Implemented the interactivity and logic in AddBar

--- Components/AddBar.tsx
+++ Components/AddBar.tsx
@@ -1,14 +1,46 @@
-import AddButton from "./AddButton";
+import React from 'react';
+interface AddBarProps {
+  newTask: string;
+  setNewTask: React.Dispatch<React.SetStateAction<string>>;
+  onAdd: () => void;
+}
 
-export default function AddBar(){
-    return (
-        <form style={{
-            position: "absolute",
-            top: "100px",
-            left: "750px",
-        }}>
-            <input 
-            type="text" placeholder="Type here..."/>           
-        </form>
-    );
+export default function AddBar({ newTask, setNewTask, onAdd }: AddBarProps) {
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onAdd();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{
+      position: "absolute",
+      top: "100px",
+      left: "750px",
+    }}>
+      <input 
+        type="text" 
+        placeholder="Type here..."
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        style={{
+          width: '150px',
+          padding: '5px',
+          marginRight: '10px'
+        }}
+      />           
+      <button 
+        type="submit"
+        style={{
+          padding: '5px 15px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '3px',
+          cursor: 'pointer'
+        }}
+      >     +
+      </button>
+    </form>
+  );
 }
--- Components/AddButton.tsx
+++
@@ -1,9 +0,0 @@
-export default function AddButton(){
-    return (
-        <button style={{
-            position: "absolute",
-            top: "95px",
-            left: "900px"
-        }}>+</button>
-    );
-}
