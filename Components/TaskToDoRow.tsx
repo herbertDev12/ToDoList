47ce788e1965bf83a572d0e29d65fdc857ded489
herbertDev12 <herbertn740@gmail.com>
2025-04-18 16:39:36 -0700

    Modified TaskToDoRow

--- Components/TaskToDoRow.tsx
+++ Components/TaskToDoRow.tsx
@@ -1,28 +1,21 @@
-import CompletionButton from "./CompletionButton";
 import DeleteButton from "./DeleteButton";
-
+import CompletionButton from "./CompletionButton";
 interface TaskToDoRowProps {
-  taskId: number;
-  taskName: string;
+  task: Task;
+  onStatusChange: (taskId: number, newStatus: TaskStatus) => void;
   onDelete: (taskId: number) => void;
 }
 
-export default function TaskToDoRow({ taskId, taskName, onDelete} : TaskToDoRowProps ) {
+export default function TaskToDoRow({ task, onStatusChange, onDelete }: TaskToDoRowProps) {
   return (
     <tr>
-      <td style={{ 
-        border: '4px double #009688',
-        padding: '8px',
-        display: 'flex',
-        gap: '10px',
-        margin: '5px 0',
-        minWidth: '10px',
-        marginLeft: '-600px',
-        justifyContent: 'space-between'
-      }}>
-        <CompletionButton/>
-        <span style={{ minWidth: '140px' }}>{taskName}</span>
-        <DeleteButton onClick={() => onDelete(taskId)} />
+      <td style={{/* tus estilos */}}>
+        <CompletionButton 
+          onClick={() => onStatusChange(task.id, 'inProgress')}
+          status="todo"
+        />
+        <span style={{ minWidth: '140px' }}>{task.name}</span>
+        <DeleteButton onClick={() => onDelete(task.id)} />
       </td>           
     </tr>
   );
