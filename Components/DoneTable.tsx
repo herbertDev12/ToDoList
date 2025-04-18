58e64c02426904e3dba9ebe6190f955d295a36b6
herbertDev12 <herbertn740@gmail.com>
2025-04-18 16:44:52 -0700

    Modified the DoneTable component

--- Components/DoneTable.tsx
+++ Components/DoneTable.tsx
@@ -1,43 +1,30 @@
 import TaskDoneRow from "./TaskDoneRow";
 
-export default function DoneTable(){
-
+// En DoneTable.tsx
+interface DoneTableProps {
+    tasks: Task[];
+    onDelete: (taskId: number) => void;
+  }
+  
+  export default function DoneTable({ tasks, onDelete }: DoneTableProps) {
     return (
-        <div>
-            
-            <table style={{
-                border: '4px double rgba(255, 255, 255, 0.7)',
-                padding: '8px',
-                background: 'rgba(255, 255, 255, 0.1)',
-                color: 'white',
-                textShadow: '0 0 3px rgba(255, 255, 255, 0.5)',
-                borderCollapse: 'collapse',
-                width: '500px',
-                margin: '20px 0',
-                position: "absolute",
-                top: "300px",
-                left: "1300px",
-            }}>
-                <thead>
-                    <tr>
-                        <th style={{  
-                            border: '4px double rgba(255, 255, 255, 0.7)', 
-                            padding: '8px',
-                            background: 'rgba(21, 15, 15, 0.1)',
-                            color: 'white',
-                            textShadow: '0 0 3px rgba(18, 17, 17, 0.5)',
-                        }}>      Done Table       </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <TaskDoneRow></TaskDoneRow>
-                    </tr>
-                    <tr>
-                        <TaskDoneRow></TaskDoneRow> 
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      <div>
+        <table style={{/* tus estilos */}}>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id}>
+                <td style={{/* tus estilos */}}>
+                  <CompletionButton 
+                    status="done"
+                    onClick={() => {}}
+                  />
+                  <span>{task.name}</span>
+                  <DeleteButton onClick={() => onDelete(task.id)} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
-}
+  }
