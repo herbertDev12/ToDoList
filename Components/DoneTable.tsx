c7eb76841010e787f549a052f33279279d447478
herbertDev12 <herbertn740@gmail.com>
2025-04-19 15:51:35 -0700

    Modified the code of DoneTable

--- Components/DoneTable.tsx
+++ Components/DoneTable.tsx
@@ -1,31 +1,28 @@
-import CompletionButton from "./CompletionButton";
-import DeleteButton from "./DeleteButton";
-import { Task } from "../types";
-// En DoneTable.tsx
+import { Task, TaskStatus } from '../types';
+import TaskRow from './TaskRow';
+
 interface DoneTableProps {
-    tasks: Task[];
-    onDelete: (taskId: number) => void;
-  }
-  
-  export default function DoneTable({ tasks, onDelete }: DoneTableProps) {
-    return (
-      <div>
-        <table style={{/* tus estilos */}}>
-          <tbody>
-            {tasks.map((task) => (
-              <tr key={task.id}>
-                <td style={{/* tus estilos */}}>
-                  <CompletionButton 
-                    status="done"
-                    onClick={() => {}}
-                  />
-                  <span>{task.name}</span>
-                  <DeleteButton onClick={() => onDelete(task.id)} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  tasks: Task[];
+  onStatusChange: (taskId: number, newStatus: TaskStatus) => void;
+  onDelete: (taskId: number) => void;
+}
+
+export default function InProgressTable({ tasks, onStatusChange, onDelete }: DoneTableProps) {
+  return (
+    <div className="column">
+      <h2>Done Table({tasks.length})</h2>
+      <table>
+        <tbody>
+          {tasks.map((task) => (
+            <TaskRow
+              key={task.id}
+              task={task}
+              onStatusChange={(newStatus) => onStatusChange(task.id, newStatus)}
+              onDelete={() => onDelete(task.id)}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
